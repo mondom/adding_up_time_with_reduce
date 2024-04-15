23add90463a16ccd77b9420e0e2d3afa37466657
Monika <codingbymon@gmail.com>
2024-04-15 14:50:29 +0200

    explanation of all methods used

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const timeNodes = [...document.querySelectorAll('[data-time]')]
 // ↑ zamiana NodeList na tablicę
-console.log(timeNodes);
+console.log(timeNodes)
+// ↑ timeNodes to wszystkie li
 
 const seconds = timeNodes.map(node => node.dataset.time)
+// ↑ za pierwszym map dostajemy się do samych wartościu atrybutów data-time czyli czasu zapisanego jako string np: "5:43"
 // .dataset daje nam dostęp do customowego atrybutu, ale musimy podać po kropce jeszcze jego drugi człon nazwy czyli w tym wypadku .time
 console.log(seconds);
+
+seconds.map(timeCode => {
+	const [mins, secs] = timeCode.split(':')
+	console.log(mins, secs)
+})
+
+//  ↑ za drugim razem map bierzemy każdy z tych stringów, rodzielamy go w miejscu : oraz przypisujemy wartość pierwszą do zmienne mins a drugą do secs.
